82ad7d6850e4d78e597c4cb5346514eef67b72ea
Zmiter <roboks@mail.ru>
2022-07-29 14:29:03 +0300

    bugfix: базовый URL вынес отдельно

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,16 +1,15 @@
-export const DOMAIN_URL = 'https://norma.nomoreparties.space';
+export const BASE_URL = 'https://norma.nomoreparties.space/api';
 
-export const INGREDIENTS_URL = DOMAIN_URL + '/api/ingredients';
-export const ORDER_URL = `${DOMAIN_URL}/api/orders`;
+export const INGREDIENTS_URL = BASE_URL + '/ingredients';
+export const ORDER_URL = `${BASE_URL}/orders`;
 
-export const REGISTER_URL = `https://norma.nomoreparties.space/api/auth/register`;
-export const LOGIN_URL = `https://norma.nomoreparties.space/api/auth/login`;
-export const LOGOUT_URL = `https://norma.nomoreparties.space/api/auth/logout`;
-export const TOKEN_URL = `https://norma.nomoreparties.space/api/auth/token`;
+export const REGISTER_URL = `${BASE_URL}/auth/register`;
+export const LOGIN_URL = `${BASE_URL}/auth/login`;
+export const LOGOUT_URL = `${BASE_URL}/auth/logout`;
+export const TOKEN_URL = `${BASE_URL}/auth/token`;
 
-export const PROFILE_URL = `https://norma.nomoreparties.space/api/auth/user`;
-export const RESET_PASSWORD_URL = `https://norma.nomoreparties.space/api/password-reset`;
-export const RESET_PASSWORD_SET_URL = `https://norma.nomoreparties.space/api/password-reset/reset`;
+export const PROFILE_URL = `${BASE_URL}/auth/user`;
+export const RESET_PASSWORD_URL = `${BASE_URL}/password-reset`;
+export const RESET_PASSWORD_SET_URL = `${BASE_URL}/password-reset/reset`;
 
 export const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-
