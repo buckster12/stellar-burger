1370afd632ac4a098d8a9be6a05069bf6c8ba754
Zmiter <roboks@mail.ru>
2022-11-22 13:05:53 +0300

    сделал правильную подсветку кнопок и иконок в хедере

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -2,15 +2,41 @@ import React from "react";
 import {Logo, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import AppHeaderStyle from './app-header.module.css';
 import classNames from "classnames";
-import {Link, NavLink} from "react-router-dom";
+import {Link, NavLink, useLocation} from "react-router-dom";
 
 function AppHeader() {
+    const location = useLocation();
+    const constructorIconClass = location.pathname === '/' ? 'primary' : 'secondary';
+    const feedIconClass = location.pathname === '/feed' ? 'primary' : 'secondary';
+
+    const ProfileIconClass = () => {
+        if (
+            location.pathname.includes('/profile') ||
+            location.pathname === '/login'
+        ) {
+            return 'primary';
+        } else {
+            return 'secondary';
+        }
+    }
+
+    const ProfileTextClass = () => {
+        if (
+            location.pathname.includes('/profile') ||
+            location.pathname === '/login'
+        ) {
+            return 'text_color_primary';
+        } else {
+            return 'text_color_inactive';
+        }
+    }
+
     return (
         <header className={AppHeaderStyle.header}>
             <nav className={AppHeaderStyle.nav}>
                 <div
                     className={classNames("text text_type_main-small m-5", AppHeaderStyle.topButton)}>
-                    <BurgerIcon type="primary"/>
+                    <BurgerIcon type={constructorIconClass}/>
                     <NavLink to="/" exact={true}
                              activeClassName="text_color_primary">
                         <span className="pl-1 pt-1">Конструктор</span>
@@ -19,7 +45,7 @@ function AppHeader() {
 
                 <div
                     className={classNames("text text_type_main-small text_color_inactive m-5", AppHeaderStyle.topButton)}>
-                    <ListIcon type="secondary"/>
+                    <ListIcon type={feedIconClass}/>
                     <NavLink to="/feed" exact={true}
                              activeClassName="text_color_primary">
                         <span className="pl-1 pt-1">Лента заказов</span>
@@ -34,8 +60,8 @@ function AppHeader() {
 
                 <div
                     className={classNames("text text_type_main-small m-5", AppHeaderStyle.topRightButton)}>
-                    <ProfileIcon type="secondary"/>
-                    <NavLink to="/profile" activeClassName="text_color_primary">
+                    <ProfileIcon type={ProfileIconClass()}/>
+                    <NavLink to="/profile" className={ProfileTextClass()}>
                         <span className="pl-1 pt-1">Личный кабинет</span>
                     </NavLink>
                 </div>
